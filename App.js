37c2ec7dbe537f0c29c9e38adccac47f4511908a
aayutha <thatheraaayushi@gmail.com>
2023-01-14 23:15:43 +0530

    error

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, Dimensions, TouchableOpacity } from 'react-native';
 import AllOrder from './Screen/AllOrder';
 import PendingOrder from './Screen/PendingOrder';
+import TopBar from './Navigators/TopBar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
     <>
-      <PendingOrder />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TopBar" component={TopBar}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+   
       {/* <AllOrder /> */}
     </>
   );
---
+++ Navigators/TopBar.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import AllOrder from '../Screen/AllOrder';
+import PendingOrder from '../Screen/PendingOrder';
+const Tab = createMaterialTopTabNavigator();
+
+const TopBar = () => {
+  return (
+    <Tab.Navigator>
+    <Tab.Screen name="AllOrder" component={AllOrder} />
+    <Tab.Screen name="PendingOrder" component={PendingOrder} />
+  </Tab.Navigator>
+  )
+}
+
+export default TopBar
+
+const styles = StyleSheet.create({})
