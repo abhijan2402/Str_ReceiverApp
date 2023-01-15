eaf1dbc3982e8103e8abe69fe2aad27121de9b22
Chirag <takchirag828@gmail.com>
2023-01-15 21:55:57 +0530

    firebase messeging

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, Dimensions, TouchableOpacity } from 'react-native';
-import AllOrder from './Screen/AllOrder';
-import PendingOrder from './Screen/PendingOrder';
 import TopBar from './Navigators/TopBar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import {NotificationListner,requestUserPermission} from './src/utils/NotificationHandlers';
 const App = () => {
   const Stack = createNativeStackNavigator();
+  useEffect(()=>{
+    requestUserPermission();
+    NotificationListner()
+  },[])
   return (
     <>
       <NavigationContainer>
